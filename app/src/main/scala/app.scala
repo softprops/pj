2531825af6629f3e14fff6cecd3b687f0cfd0c14
softprops <d.tangren@gmail.com>
2012-01-21 16:42:13 -0500

    be explicit about reading from pipe

--- app/src/main/scala/app.scala
+++ app/src/main/scala/app.scala
@@ -1,21 +1,34 @@
 package pj 
 
 object App {
+  import java.io.{ InputStream, OutputStream }
+
   def main(args: Array[String]) {
     val exit = run(args)
     System.exit(exit)
   }
 
-  def run(args: Array[String]): Int = {
-    if(System.in.available > 0) {
-      PJ(System.in, System.out).fold({ e =>
-        Console.err.println(e)
-        1
-      }, { u: Unit => 0 })
-    } else {
-      println("todo...")
-      0
+  private def piped(in: InputStream, out: OutputStream): Int = {
+    @annotation.tailref
+    def check: Int = System.in.available match {
+      case n if(n > 0) =>
+        PJ(System.in, System.out).fold({ e =>
+          Console.err.println(e)
+          1
+        }, { u: Unit => 0 })
+      case _ => check
     }
+    check
+  }
+
+  private def argumented(args: Seq[String]) = {
+    println("todo...")
+    0
+  }
+
+  def run(args: Array[String]): Int = {
+    if(args.contains("-p")) piped(System.in, System.out)
+    else argumented(args)
   }
 }
 
